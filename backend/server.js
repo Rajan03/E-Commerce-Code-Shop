5ea553d758541c203df1133e657952bc5630274a
Rajan Verma <rajvrm003@gmail.com>
2021-11-04 23:13:35 +0530

    Deploy Setup

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import productRoutes from "./routes/productsRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
-import { notFound, errorHandler } from "./middlewares/errorHandler.js";
+import {notFound, errorHandler} from "./middlewares/errorHandler.js";
 import path from "path";
 
 // Initialize App
@@ -29,9 +29,17 @@ app.use("/api/upload", uploadRoutes)
 
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
+
 // __dirname is only available with commonJS syntax but is not accessible in ES6 Modules.
 const __dirname = path.resolve();
-app.use(`/uploads`, express.static(path.join(__dirname,'/uploads')));
+app.use(`/uploads`, express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
+
 
 // Middlewares
 app.use(notFound);
